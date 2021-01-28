8071a56564a77d75da705d34a9f3874e125727e1
Moath <77831566+moathappslop@users.noreply.github.com>
2021-01-28 14:00:35 +0300

    Initial commit

---
+++ app/src/main/java/com/moath/mygoal/DailyForecastListAdapter.kt
@@ -0,0 +1,50 @@
+package com.moath.mygoal
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class DailyForecastViewHolder(view: View) : RecyclerView.ViewHolder(view){
+
+    private val tempText: TextView = view.findViewById<TextView>(R.id.tempText)
+    private val decriptionText: TextView = view.findViewById(R.id.descriptionText)
+
+    fun bind(dailyForecast: DailyForecast) {
+        tempText.text = String.format("%.2f", dailyForecast.temp)
+        decriptionText.text = dailyForecast.description
+    }
+}
+
+class DailyForecastAdapter(
+    private val clickHandler: (DailyForecast) -> Unit
+) : ListAdapter<DailyForecast, DailyForecastViewHolder>(DIEF_CONFIG) {
+
+    companion object {
+        val DIEF_CONFIG = object: DiffUtil.ItemCallback<DailyForecast>() {
+            override fun areItemsTheSame(oldItem: DailyForecast, newItem: DailyForecast): Boolean {
+                return oldItem === newItem
+            }
+
+            override fun areContentsTheSame(oldItem: DailyForecast, newItem: DailyForecast): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DailyForecastViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_daily_forecast, parent,false)
+        return DailyForecastViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: DailyForecastViewHolder, position: Int) {
+        holder.bind(getItem(position))
+        holder.itemView.setOnClickListener {
+            clickHandler(getItem(position))
+        }
+    }
+}
---
+++ app/src/main/java/com/moath/mygoal/details/ForecastDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.moath.mygoal.details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ForecastDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forecast_details)
+    }
+}
